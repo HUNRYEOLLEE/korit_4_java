2aea8cd7993c7bca5230ec20c6915d6d64b8c84e
HUNRYEOLLEE <twinshun3402@naver.com>
2025-04-23 14:03:56 +0900

    feat: 20250422 / java_started

---
+++ src/main/java/ch06_condition/Condition07.java
@@ -0,0 +1,63 @@
+package ch06_condition;
+
+import java.util.Scanner;
+
+/*
+     중첩 if문(Nested - if)
+       if 문 내에 if문이 연속적으로 작성될 수 있습니다.
+
+     형식 :
+     if(조건식1) {
+        실행문1
+        if(조건식1a) {
+            실행문1a
+        } else if(조건식1b) {
+            실행문1b
+        } else if(조건식1C) {
+            실행문1c
+        } else {
+            실행문1d
+        }
+     } else if(조건식2) {
+        실행문2
+        if(조건식2a) {
+            실행문2a
+        } else(조건식2b) {
+            실행문2b
+        }
+     } else {
+        실행문3
+        if(조건식3a) {
+            실행문3a
+        }
+     }
+ */
+public class Condition07 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int score;
+        String grade;
+
+        System.out.print("점수를 입력하세요 >>> ");
+        score = scanner.nextInt();
+
+        // 중첩 if문을 적용한 조건문 작성
+        if(score > 100 || score < 0){
+            System.out.println("잘못된 점수 입력입니다.");
+            grade = "X";
+        } else {    // 0보다는 크거나 같고 100보다는 작거나 같은 score 범위 : 0 <= score <= 100
+            if (score > 89) {
+                grade = "A";
+            } else if(score > 79){
+                grade = "B";
+            } else if(score > 69){
+                grade = "C";
+            } else if (score > 59) {
+                grade = "D";
+            } else {
+                grade = "F";
+            }
+        }
+        System.out.println("당신의 점수는 " + score + "점이고, 학점은 " + grade + "입니다.");
+    }
+}
